d7b05603558a5ef1bde87a0fd2bc6bf737be6485
AnaMMoreira <cinzasdanjo@hotmail.com>
2022-07-21 05:40:41 -0400

    module 11 change
    code update /final

--- static/js/app.js
+++ static/js/app.js
@@ -32,8 +32,9 @@ function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
     let changedElement = d3.select(this);
+    //console.log(changedElement)
     // 4b. Save the value that was changed as a variable.
-    let elementValue = changedElement.property("value")
+    let elementValue = changedElement.property("value");
     console.log(elementValue);
     // 4c. Save the id of the filter that was changed as a variable.
     let filterId = changedElement.attr("id");
@@ -41,13 +42,13 @@ function updateFilters() {
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
-    if (elementValue != "") {
+    if (elementValue) {
       filters[filterId] = elementValue;
     }
     else {
       delete filters[filterId];
     }
-    //console.log(filters);
+    console.log(filters[filterId]);
 
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
@@ -64,18 +65,16 @@ function updateFilters() {
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
 
-    for(filterId in filters) {
-
-      filteredData = filteredData.filter(row => row.filterId === elementValue);
+    for (var keys in filters) {
+      filteredData = filteredData.filter(row => row[keys] === filters[keys])
     }
-
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
-  //  d3.selectAll("input").on("change", updateFilters);
-    d3.selectAll("#filter-btn").on("click", updateFilters);
+  d3.selectAll("input").on("change",updateFilters);
+  //  d3.selectAll("#filter-btn").on("click", updateFilters);
   
   // Build the table when the page loads
   buildTable(tableData);
